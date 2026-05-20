af624dec797a46441fd525fcade1804e9137e923
alarmlie <alarmlie@student.21-school.ru>
2026-05-20 23:09:39 +0300

    ci обнова

--- service_a/conftest.py
+++ service_a/conftest.py
@@ -1,10 +1,8 @@
 import sys
 from pathlib import Path
+import pytest
 
-# Добавляем корень проекта в sys.path
 sys.path.insert(0, str(Path(__file__).parent))
-
-# Фикстура для асинхронных тестов (если понадобятся)
 
 @pytest.fixture
 def anyio_backend():
